373c67143af86c2e15f7fee894f0489bb7cf5ee4
qtaro3400 <poohme3400@gmail.com>
2023-08-26 04:04:58 +0900

    Add: main.tf用意、VPCとサブネットを作成

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = "~> 1.5.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.48.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+  default_tags {
+    tags = {
+      Terraform = "true"
+    }
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,91 @@
+# -----------------------------
+# VPC
+# -----------------------------
+resource "aws_vpc" "vpc_main" {
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+  tags = {
+    Name = "${var.project}-${var.env}-vpc"
+  }
+}
+
+# -----------------------------
+# Subnet
+# -----------------------------
+resource "aws_subnet" "public_1a" {
+  vpc_id                  = aws_vpc.vpc_main.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.10.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.project}-${var.env}-subnet-public-1a"
+  }
+}
+
+resource "aws_subnet" "public_1c" {
+  vpc_id                  = aws_vpc.vpc_main.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.20.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "${var.project}-${var.env}-subnet-public-1c"
+  }
+}
+
+resource "aws_subnet" "private_1a" {
+  vpc_id                  = aws_vpc.vpc_main.id
+  availability_zone       = "ap-northeast-1a"
+  cidr_block              = "192.168.30.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.project}-${var.env}-subnet-private-1a"
+  }
+}
+
+resource "aws_subnet" "private_1c" {
+  vpc_id                  = aws_vpc.vpc_main.id
+  availability_zone       = "ap-northeast-1c"
+  cidr_block              = "192.168.40.0/24"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "${var.project}-${var.env}-subnet-private-1c"
+  }
+}
+
+# -----------------------------
+# Route Table
+# -----------------------------
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.vpc_main.id
+  tags = {
+    Name = "${var.project}-${var.env}-rt-public"
+  }
+}
+
+resource "aws_route_table_association" "rta_public_1a" {
+  route_table_id = aws_route_table.rt_public.id
+  subnet_id      = aws_subnet.public_1a.id
+}
+
+resource "aws_route_table_association" "rta_public_1c" {
+  route_table_id = aws_route_table.rt_public.id
+  subnet_id      = aws_subnet.public_1c.id
+}
+
+resource "aws_route_table" "rt_private" {
+  vpc_id = aws_vpc.vpc_main.id
+  tags = {
+    Name = "${var.project}-${var.env}-rt-private"
+  }
+}
+
+resource "aws_route_table_association" "rta_private_1a" {
+  route_table_id = aws_route_table.rt_private.id
+  subnet_id      = aws_subnet.private_1a.id
+}
+
+resource "aws_route_table_association" "rta_private_1c" {
+  route_table_id = aws_route_table.rt_private.id
+  subnet_id      = aws_subnet.private_1c.id
+}
